3a444452cecdb60ec15bce8c07aa2cb891d71db4
mechanik20051988 <mechanik20051988@tarantool.org>
2021-12-22 21:36:55 +0300

    msgpack: add msgpack support for compressed data.
    Implement new MP_EXT type MP_COMPRESSION for compressed msgpack
    fields. First byte encodes compression type, second eight bytes
    encode size of uncompressed data then an array of compressed
    data is stored. If compressed data size is greater or equal than
    uncompressed data size, then uncompressed data is stored during
    compression. Currently there are only one supported compression
    type: COMPRESSION_TYPE_ZSTD - zstd compression with various
    compression levels.
    
    Part of #2695

---
+++ src/lib/core/tt_compression.h
@@ -0,0 +1,52 @@
+#pragma once
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 2021, Tarantool AUTHORS, please see AUTHORS file.
+ */
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+enum compression_type {
+	COMPRESSION_TYPE_NONE = 0,
+	COMPRESSION_TYPE_ZSTD,
+	compression_type_MAX
+};
+
+/**
+ * Compress data from @a src data array into @a dst data array
+ * according to compression @a type and @a level. Return 0 in
+ * case of error, or compressed data size if success.
+ */
+size_t
+tt_compress(char *dst, size_t dst_size, const char *src,
+	    size_t src_size, enum compression_type type,
+	    uint32_t level);
+
+/**
+ * Decompress data from @src data array to @a dst data array
+ * according to compression @a type. Return 0 in case of error,
+ * or decompressed data size if success.
+ */
+size_t
+tt_decompress(char *dst, size_t dst_size, const char *src,
+	      size_t src_size, enum compression_type type);
+
+/**
+ * Check that compression type is valid.
+ */
+static inline bool
+tt_check_compression_type(enum compression_type type)
+{
+        return (type > COMPRESSION_TYPE_NONE && type < compression_type_MAX);
+}
+
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif /* defined(__cplusplus) */
