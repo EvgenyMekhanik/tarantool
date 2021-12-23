f80c8e1119962877f67335e4e0c07f41dc7a60c8
mechanik20051988 <mechanik20051988@tarantool.org>
2021-12-23 18:05:26 +0300

    msgpack: fixed incorrect print of msgpack field
    Due to the fact that we used `const char**` to save a pointer to the
    original msgpack header, when restoring the pointer, we received an
    incorrect value and as a consequence assertion failure later. Correctly
    saving and restoring the pointer fixed this problem.

---
+++ test/unit/fix-mp-ext-print.c
@@ -0,0 +1,36 @@
+#include "msgpack.h"
+#include "mp_extension_types.h"
+#include "trivia/util.h"
+#include "unit.h"
+#include <stdio.h>
+
+int
+main(void)
+{
+	plan(2);
+
+	msgpack_init();
+
+	char expected[] = "(extension: type 0, len 10)";
+	char got[sizeof(expected)] = {0};
+	char data[] = { 0xca, 0xca, 0xca, 0xca, 0xca, 0xca, 0xca, 0xca, 0xca, 0xca };
+
+	uint32_t size = mp_sizeof_ext(sizeof(data));
+	char *ext_data = xmalloc(size);
+	char *data_end = ext_data;
+	data_end = mp_encode_ext(data_end, MP_UNKNOWN_EXTENSION, data, sizeof(data));
+	mp_snprint(got, sizeof(got), ext_data);
+	is(strcmp(expected, got), 0, "mp_snprint");
+
+	memset(got, 0, sizeof(got));
+	FILE *f  = tmpfile();
+	assert(f != NULL);
+	mp_fprint(f, ext_data);
+	rewind(f);
+	fread(got, 1, sizeof(got), f);
+	is(strcmp(expected, got), 0, "mp_fprint");
+
+	free(ext_data);
+
+	return check_plan();
+}
